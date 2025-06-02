66d258afb17e416b0f6ee53d9c6a456cbf3ae449
Abhishek Mishra <mishraabhishek0807@gmail.com>
2025-06-02 10:13:46 +0530

    Added mapping and better css

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,77 @@
-import "./App.css";
-import coreConceptImage from "./assets/react-core-concepts.png";
-import components from "./assets/components.png";
-import jsx from "./assets/jsx-ui.png";
-import state from "./assets/state-mgmt.png";
-import props from "./assets/config.png";
+import { useState } from "react";
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./component/Header/Header";
 import CoreConcept from "./component/CoreConcept";
 import TabButton from "./component/TabButton";
-import { EXAMPLES } from "./data";
-import { useState } from "react";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleClick(topic) {
-    setSelectedTopic(topic);
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+  }
+
+  console.log("APP COMPONENT EXECUTING");
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
     <div>
-      <header>
-        <img src={coreConceptImage} alt="core concept" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
-      <main id="tab-content">
-        <h2>Core Concepts</h2>
-        <div>
-          <CoreConcept
-            title="Component"
-            description="Components are the basic building blocks of a React application's user interface (UI). They are independent, reusable pieces of code that encapsulate a specific part of the UI and its behavior."
-            image={components}
-          />
-          <CoreConcept
-            title="JSX"
-            description="State in React refers to a JavaScript object that stores data specific to a component. This data can change over time, typically due to user interactions, API responses, or other events."
-            image={jsx}
-          />
-          <CoreConcept
-            title="Props"
-            description="In React, props (short for properties) are a mechanism for passing data from a parent component to its child components. They are essentially inputs to a component that allow it to receive and display dynamic information.
-"
-            image={props}
-          />
-          <CoreConcept
-            title="State"
-            description="State in React refers to a JavaScript object that stores data specific to a component. This data can change over time, typically due to user interactions, API responses, or other events."
-            image={state}
-          />
-        </div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
-
-      <section>
-        <h2>Examples</h2>
-        <menu id="tab-content">
-          <TabButton
-            isSelected={selectedTopic === "components"}
-            onSubmit={() => handleClick("components")}
-          >
-            Components
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "jsx"}
-            onSubmit={() => handleClick("jsx")}
-          >
-            JSX
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "props"}
-            onSubmit={() => handleClick("props")}
-          >
-            Props
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "state"}
-            onSubmit={() => handleClick("state")}
-          >
-            State
-          </TabButton>
-        </menu>
-
-        {!selectedTopic ? (
-          <p>Please select a Topic</p>
-        ) : (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        )}
-      </section>
     </div>
   );
 }
